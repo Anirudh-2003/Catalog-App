fe2f159fd77ccbfa93037afd3eb5f6f24c8ce275
Anirudh <anirudh.bhosle2003@gmail.com>
2023-10-08 10:55:39 +0530

    Local Files | Load & Decode JSON

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,24 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:untitled/models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  void initState() {
+      super.initState();
+      loadData();
+  }
+
+  loadData() async
+  {
+    var json = await rootBundle.loadString("Assests/Files/catalog.json");
+    final decodeData = jsonDecode(json);
+    var productsData = decodeData["products"];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate((50), (index) => CatalogModel.items[0]);
+    final dummyList = List.generate((50), (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Learn Flutter"),
+        title: Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: ListView.builder(
-          itemCount:  dummylist.length,
+          itemCount:  dummyList.length,
           itemBuilder: (context,index)  {
-            return ItemWidget(item: dummylist[index]);
+            return ItemWidget(item: dummyList[index]);
           },
         ),
       ),
